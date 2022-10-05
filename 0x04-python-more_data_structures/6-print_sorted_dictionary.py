080a1b591c256e33b91c76d1f7dfb9b0b147edcc
Krystian Maccs <krystianmaccs@gmail.com>
2022-10-05 06:11:30 -0700

    minor fixes

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def number_keys(my_dict):
-    return len(my_dict)
+def print_sorted_dictionary(my_dict):
+    for key in sorted(my_dict.keys()):
+        print("{:s}: {}".format(key, my_dict[key]))
